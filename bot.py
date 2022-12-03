c58f3ffa151e2f79ababf73dedc3922f032d654b
userwei <chou.ting.wei@outlook.com>
2022-12-03 21:47:14 +0800

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+import responses
+import json
+
+with open('setting.json', mode='r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+        
+def run_discord_bot():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents = intents)
+    
+    @client.event
+    async def on_ready():
+        print('Bot is online!')
+        print('user:', client.user)
+        
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+        
+        print(f"{username} said: '{user_message}' ({channel})")
+        
+        if user_message[0] == '%':
+            user_message = user_message[1:]
+            response = responses.handle_response(user_message)
+            await message.channel.send(response)
+        
+    client.run(jdata['TOKEN'])
+
+
+
+
+# @bot.command()
+# async def ping(ctx):
+#     await ctx.send(f'{round(bot.latency*1000)} (ms)')
---
+++ responses.py
@@ -0,0 +1,15 @@
+import random
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+    
+    if p_message == 'hello':
+        return 'Hi!'
+    
+    if p_message == 'roll':
+        return str(random.randint(1, 6))
+    
+    if p_message == 'help':
+        return 'cool'
+    
+    return 'Do you speak chinese?'
