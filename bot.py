7af48a6242fb0aba10251f86dcdc0c2272125a36
userwei <chou.ting.wei@outlook.com>
2022-12-04 02:50:30 +0800

    add schedule daily message

--- bot.py
+++ bot.py
@@ -1,25 +1,42 @@
 import discord
+from discord.ext import commands
 import responses
 import json
+import datetime
+import asyncio
+# import keep_alive
 
 with open('setting.json', mode='r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
-        
-def run_discord_bot():
+    
+def run_discord_bot():    
     intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents = intents)
+    bot = commands.Bot(intents = intents, command_prefix = '%')
     
-    @client.event
+    async def schedule_daily_message():
+        while True:
+            now = datetime.datetime.now()
+            midnight = (now + datetime.timedelta(days = 1)).replace(hour=0, minute=0, second=0, microsecond=0)
+            wait_time = (midnight - now).total_seconds()
+            print('Next daily message:', wait_time, '(s)')
+            await asyncio.sleep(wait_time)
+            
+            channel = bot.get_channel(1043935672207159316)
+            response = responses.handle_response('countdown')
+            await channel.send(response)
+    
+    @bot.event
     async def on_ready():
         print('Bot is online!')
-        print('Logged in as', client.user)
+        print('Logged in as', bot.user)
         game = discord.Game('>_<')
-        await client.change_presence(status = discord.Status.idle, activity = game)
+        await bot.change_presence(status = discord.Status.idle, activity = game)
+        await schedule_daily_message()
         
-    @client.event
+    @bot.event
     async def on_message(message):
-        if message.author == client.user:
+        if message.author == bot.user:
             return
         
         username = str(message.author)
@@ -33,7 +50,8 @@ def run_discord_bot():
             response = responses.handle_response(user_message)
             await message.channel.send(response)
         
-        if client.user.mentioned_in(message):
+        if bot.user.mentioned_in(message):
             await message.channel.send('You can type `%help` for more info.')
         
-    client.run(jdata['TOKEN'])
+    # keep_alive.keep_alive()
+    bot.run(jdata['TOKEN'])
